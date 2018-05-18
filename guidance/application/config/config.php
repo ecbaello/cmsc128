6081946c92208eddbe6c10e6b23beb7105e3e0bb
mixx <ecbaello@up.edu.ph>
2018-05-18 22:55:36 +0800

    added page num

--- guidance/application/config/config.php
+++ guidance/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/cmsc128/guidance';
+$config['base_url'] = 'http://localhost/cmsc-128/guidance';
 
 /*
 |--------------------------------------------------------------------------
